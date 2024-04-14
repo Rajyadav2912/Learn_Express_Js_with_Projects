055b25177ba769c008e8c0c23c209e92146e3961
Raj Yadav <rajyadav7047@gmail.com>
2024-04-14 16:20:37 +0530

    test api commit

--- 04CreateRestAPI/controller/projectsContoller.js
+++ 04CreateRestAPI/controller/projectsContoller.js
@@ -1,7 +1,8 @@
 const Product = require("../model/productModel");
+const { link } = require("../routes/productRoutes");
 
 const getProducts = async (req, res) => {
-  const { company, name, featured } = req.query;
+  const { company, name, featured, sort, select } = req.query;
   const queryObject = {};
 
   if (company) {
@@ -18,15 +19,35 @@ const getProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
+  let apiData = Product.find(queryObject);
+
+  if (sort) {
+    let sortfix = sort.replace(",", " ");
+    apiData = apiData.sort(sortfix);
+  }
+
+  if (select) {
+    let selectfix = select.split(",").join(" ");
+
+    apiData = apiData.select(selectfix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+
+  let skip = (page - 1) * limit;
+
+  apiData = apiData.skip(skip).limit(limit);
+
   console.log(queryObject);
 
-  const myData = await Product.find(queryObject);
+  const myData = await apiData;
 
   res.status(200).json({ myData });
 };
 
 const getProductsTesting = async (req, res) => {
-  const myData = await Product.find(req.query);
+  const myData = await Product.find(req.query).sort("name -price");
   console.log("😍 File: products.Js ~ Line 10 : ", req.query);
 
   res.status(200).json({ myData });
